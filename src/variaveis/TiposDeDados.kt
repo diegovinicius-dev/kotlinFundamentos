c1cc888276fca0d1713702025093e9eca79edfd5
Diego Vinícius <diego.cavalcante90@gmail.com>
2025-02-27 11:55:46 -0300

    Adicionando Tipos de Dados

---
+++ src/variaveis/TiposDeDados.kt
@@ -0,0 +1,44 @@
+package variaveis
+
+fun main() {
+    // Tipos numericos
+    /*
+    * Byte - 8 bits
+    * Short - 16 bits
+    * Int - 32 bits
+    * Long - 64 bits
+    * Float - 32 bits
+    * Double - 64
+    * */
+
+    val byte: Byte = 8
+    val short: Short = 360
+    val int = 1000
+    val long: Long = 10000L
+    val float: Float = 10.10F
+    val double: Double = 300.10
+
+    println(byte)
+    println(short)
+    println(int)
+    println(long)
+    println(float)
+    println(double)
+
+    // Tipo Boolean
+    val boolean: Boolean = true
+    println(boolean)
+
+    // Tipo Char - tipos textuais
+    /*
+    * Char - guarda um único caracter
+    * String - textos
+    * */
+
+    val char: Char = 'D'
+    val string: String = "Diego"
+
+    println(char)
+    println(string)
+
+}
