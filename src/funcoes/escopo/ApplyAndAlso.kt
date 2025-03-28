29194f1586500e44a760f40787fe4843f002d86e
Diego Vinicius <diegocavalcante.tech@gmail.com>
2025-03-28 15:13:43 -0300

    fix: Altera nome da função

--- src/funcoes/escopo/ApplyAndAlso.kt
+++ src/funcoes/escopo/ApplyAndAlso.kt
@@ -1,16 +1,16 @@
 package funcoes.escopo
 
-data class Person(var name: String, var age: Int, var about: String) {
+data class ApplyAndAlso(var name: String, var age: Int, var about: String) {
     constructor() : this("", 0, "")
 }
 
-fun writeCreationLog(p: Person) {
+fun writeCreationLog(p: ApplyAndAlso) {
     println("Uma nova pessoa ${p.name} foi criada.")
 }
 
 fun main() {
 
-    val jake = Person()
+    val jake = ApplyAndAlso()
     val stringDescription = jake.apply {
         name = "Jake"
         age = 30
@@ -19,7 +19,7 @@ fun main() {
     println(stringDescription)
 
     // Função Also
-    val mike = Person("Mike", 25, "Android developer")
+    val mike = ApplyAndAlso("Mike", 25, "Android developer")
         .also {
             writeCreationLog(it)
         }
